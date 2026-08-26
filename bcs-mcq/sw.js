cba848b2ed72452ef9224f7c0b3b7d8cc7c1bec6
Selim Hossen <openjobsolutionbd@gmail.com>
2026-08-26 11:30:41 +0600

    chore: bump version to v1.154
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- bcs-mcq/sw.js
+++ bcs-mcq/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'bcs-';
-const CACHE_VERSION = CACHE_PREFIX + 'v1.153';
+const CACHE_VERSION = CACHE_PREFIX + 'v1.154';
 
 const ASSETS = [
   '/bcs-mcq/',
--- written-exam/sw.js
+++ written-exam/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'written-';
-const CACHE_VERSION = CACHE_PREFIX + 'v1.153';
+const CACHE_VERSION = CACHE_PREFIX + 'v1.154';
 
 const ASSETS = [
   '/written-exam/',

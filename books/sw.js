477bf13efecbd66e1e80feefb7a0560730a10bf2
Selim Hossen <openjobsolutionbd@gmail.com>
2026-08-28 13:42:02 +0600

    chore: bump version to v1.178
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- books/sw.js
+++ books/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'books-';
-const CACHE_VERSION = CACHE_PREFIX + 'v1.177';
+const CACHE_VERSION = CACHE_PREFIX + 'v1.178';
 
 const ASSETS = [
   '/books/',
--- written-exam/sw.js
+++ written-exam/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'written-';
-const CACHE_VERSION = CACHE_PREFIX + 'v1.177';
+const CACHE_VERSION = CACHE_PREFIX + 'v1.178';
 
 const ASSETS = [
   '/written-exam/',

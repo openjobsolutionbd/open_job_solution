1f346fe4cd87eb1d8e4aaaf10327f52f8a1b35f6
Selim Hossen <openjobsolutionbd@gmail.com>
2026-08-29 23:22:30 +0600

    chore: bump version to v1.198
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- books/sw.js
+++ books/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'books-';
-const CACHE_VERSION = CACHE_PREFIX + 'v1.197';
+const CACHE_VERSION = CACHE_PREFIX + 'v1.198';
 
 const ASSETS = [
   '/books/',
--- primary-mcq/sw.js
+++ primary-mcq/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'primary-';
-const CACHE_VERSION = CACHE_PREFIX + 'v1.197';
+const CACHE_VERSION = CACHE_PREFIX + 'v1.198';
 
 const ASSETS = [
   '/primary-mcq/',

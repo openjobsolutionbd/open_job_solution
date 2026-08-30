83b064e5b6d7d4e0f25ccfa895c897e58d52e9d3
Selim Hossen <openjobsolutionbd@gmail.com>
2026-08-30 11:29:15 +0600

    chore: bump version to v1.207
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- bcs-mcq/sw.js
+++ bcs-mcq/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'bcs-';
-const CACHE_VERSION = CACHE_PREFIX + 'v1.206';
+const CACHE_VERSION = CACHE_PREFIX + 'v1.207';
 
 const ASSETS = [
   '/bcs-mcq/',
--- books/sw.js
+++ books/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'books-';
-const CACHE_VERSION = CACHE_PREFIX + 'v1.206';
+const CACHE_VERSION = CACHE_PREFIX + 'v1.207';
 
 const ASSETS = [
   '/books/',

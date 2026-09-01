9988de12331bd961ff0c0e56d2d565303b5e9b54
Selim Hossen <openjobsolutionbd@gmail.com>
2026-09-01 12:21:20 +0600

    chore: bump version to v1.226
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- bcs-mcq/sw.js
+++ bcs-mcq/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'bcs-';
-const CACHE_VERSION = CACHE_PREFIX + 'v1.225';
+const CACHE_VERSION = CACHE_PREFIX + 'v1.226';
 
 const ASSETS = [
   '/bcs-mcq/',
--- primary-mcq/sw.js
+++ primary-mcq/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'primary-';
-const CACHE_VERSION = CACHE_PREFIX + 'v1.225';
+const CACHE_VERSION = CACHE_PREFIX + 'v1.226';
 
 const ASSETS = [
   '/primary-mcq/',

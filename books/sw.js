74268d13ed4dc75d1606000a1e7d583614616f78
Selim Hossen <openjobsolutionbd@gmail.com>
2026-09-26 15:05:50 +0600

    chore: bump version to v1.295
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- books/sw.js
+++ books/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'books-';
-const CACHE_VERSION = CACHE_PREFIX + 'v1.294';
+const CACHE_VERSION = CACHE_PREFIX + 'v1.295';
 
 const ASSETS = [
   '/books/',
--- written-exam/sw.js
+++ written-exam/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'written-';
-const CACHE_VERSION = CACHE_PREFIX + 'v1.294';
+const CACHE_VERSION = CACHE_PREFIX + 'v1.295';
 
 const ASSETS = [
   '/written-exam/',
